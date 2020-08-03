bef12b45aea54ea1b3f798fc92248b1f59d4a823
Leonidas <web@pirron.one>
2020-08-03 16:59:07 +0200

    Updates to water effects config options

--- dist/scripts/uranus-scripts.js
+++ dist/scripts/uranus-scripts.js
@@ -2365,6 +2365,31 @@ UranusEffectWater.attributes.add("waveWidth", {
     type: "number",
     default: 0.1,
 });
+UranusEffectWater.attributes.add("waveFrequency", {
+    type: "number",
+    default: 1.0,
+    min: 0.0,
+});
+UranusEffectWater.attributes.add("waveFalloff", {
+    type: "number",
+    default: 2.0,
+    min: 0.0,
+});
+UranusEffectWater.attributes.add("depthFactor", {
+    type: "number",
+    default: 2.0,
+    min: 1.0,
+});
+UranusEffectWater.attributes.add("depthDiscard", {
+    type: "number",
+    default: 1.0,
+    min: 0.0,
+    max: 1.0,
+});
+UranusEffectWater.attributes.add("autoUpdate", {
+    type: "boolean",
+    default: false,
+});
 UranusEffectWater.prototype.initialize = function () {
     // --- shader
     this.chunkSources = this.getChunkSourcesShader();
@@ -2513,6 +2538,7 @@ UranusEffectWater.prototype.updateWater = function () {
         pc.drawFullscreenQuad(this.app.graphicsDevice, this.renderTargetA, this.vertexBuffer, this.quadShaderBlurVertical);
     }
     this.material.diffuseMap = this.textureA;
+    this.material.diffuseMapTiling = new pc.Vec2(1, 1);
     this.material.normalDetailMap = this.depthMap.resource;
     this.material.update();
     this.updateUniforms();
@@ -2520,7 +2546,11 @@ UranusEffectWater.prototype.updateWater = function () {
 };
 UranusEffectWater.prototype.updateUniforms = function () {
     this.material.setParameter("waveWidth", this.waveWidth);
+    this.material.setParameter("waveFrequency", this.waveFrequency);
+    this.material.setParameter("waveFalloff", this.waveFalloff);
     this.material.setParameter("landWidth", this.landWidth);
+    this.material.setParameter("depthFactor", this.depthFactor);
+    this.material.setParameter("depthDiscard", this.depthDiscard);
     this.material.setParameter("colorWater", this.colorWater.data3);
     this.material.setParameter("colorWave", this.colorWave.data);
 };
@@ -2528,7 +2558,7 @@ UranusEffectWater.prototype.updateUniforms = function () {
 UranusEffectWater.prototype.update = function (dt) {
     if (!this.material)
         return;
-    if (this.dirty || this.material.dirty) {
+    if (this.autoUpdate === true || this.dirty || this.material.dirty) {
         this.dirty = false;
         this.updateWater();
     }
@@ -2586,22 +2616,26 @@ UranusEffectWater.prototype.getWaterShader = function () {
         "uniform sampler2D texture_normalDetailMap;\n" +
         "uniform float time;\n" +
         "uniform float waveWidth;\n" +
+        "uniform float waveFrequency;\n" +
+        "uniform float waveFalloff;\n" +
         "uniform float landWidth;\n" +
+        "uniform float depthFactor;\n" +
+        "uniform float depthDiscard;\n" +
         "uniform vec3 colorWater;\n" +
         "uniform vec4 colorWave;\n" +
         "void getAlbedo() {\n" +
-        "    vec3 depth = clamp( texture2D(texture_normalDetailMap, vUv0).rgb, 0.0, 0.5) * 2.0;\n" +
+        "    vec3 depth = clamp( texture2D(texture_normalDetailMap, vec2(vUv0.x, 1.0 - vUv0.y)).rgb, 0.0, depthDiscard);\n" +
         "    dDiffuseLight = vec3(1.0);\n" +
-        "    dAlbedo = mix(colorWater / 2.0, colorWater, depth);\n" +
+        "    dAlbedo = mix(colorWater / depthFactor, colorWater, depth);\n" +
         "    vec3 base = texture2DSRGB(texture_diffuseMap, $UV).rgb;\n" +
         "    float b = mod((base.x / 2.0 + base.y / 2.0 + base.z) * 2.0, 1.0);\n" +
-        "    float off = ((sin(vPositionW.x * 1.0) + 1.0) + (cos(vPositionW.z * 1.0) + 1.0)) / 4.0;// + time;\n" +
+        "    float off = ((sin(vPositionW.x * waveFrequency) + 1.0) + (cos(vPositionW.z * waveFrequency) + 1.0)) / 4.0;// + time;\n" +
         "    float t = 1.0 - pow(1.0 - mod(time + off, 1.0), 0.3);\n" +
         "    float thickness = max(0.0, pow(t * 2.0, 2.0));\n" +
         "    if (base.z > 0.3) {\n" +
         "        dAlbedo = mix(dAlbedo, colorWave.rgb, colorWave.a);\n" +
         "    } else if (t < b && t > (b - (waveWidth * thickness)) && b > 0.01) {\n" +
-        "        float o = 1.0 - pow(1.0 - t, 4.0);\n" +
+        "        float o = 1.0 - pow(1.0 - t, waveFalloff);\n" +
         "        dAlbedo = mix(dAlbedo, colorWave.rgb, colorWave.a * o);\n" +
         "    }\n" +
         "    dAlbedo.rgb += base * landWidth;\n" +

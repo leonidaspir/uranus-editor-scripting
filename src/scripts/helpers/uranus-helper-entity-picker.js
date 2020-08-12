1bae878f0d7678a7bdf4f17263041f4ca4ea7f60
Leonidas <web@pirron.one>
2020-08-12 10:19:40 +0200

    Added support for touch input

--- src/scripts/helpers/uranus-helper-entity-picker.js
+++ src/scripts/helpers/uranus-helper-entity-picker.js
@@ -27,28 +27,57 @@ UranusHelperEntityPicker.prototype.initialize = function () {
     this.app.graphicsDevice.canvas.width,
     this.app.graphicsDevice.canvas.height
   );
-  this.app.mouse.on(pc.EVENT_MOUSEUP, this.onSelect, this);
+  this.app.mouse.on(pc.EVENT_MOUSEDOWN, this.onMouseDown, this);
+  this.app.mouse.on(pc.EVENT_MOUSEUP, this.onMouseUp, this);
 
   if (this.app.touch) {
-    this.app.touch.on(pc.EVENT_TOUCHEND, this.onSelect, this);
+    this.app.touch.on(pc.EVENT_TOUCHSTART, this.onTouchStart, this);
+    this.app.touch.on(pc.EVENT_TOUCHEND, this.onTouchEnd, this);
   }
 
   // --- events
   this.app.graphicsDevice.on("resizecanvas", this.onResize.bind(this));
 };
 
+UranusHelperEntityPicker.prototype.onMouseDown = function (e) {
+  e.event.preventDefault();
+
+  this.onSelect(e, "clickDown");
+};
+
+UranusHelperEntityPicker.prototype.onMouseUp = function (e) {
+  e.event.preventDefault();
+
+  this.onSelect(e, "click");
+};
+
+UranusHelperEntityPicker.prototype.onTouchStart = function (e) {
+  this.onSelect(e.touches[0], "clickDown");
+
+  e.event.preventDefault();
+};
+
+UranusHelperEntityPicker.prototype.onTouchEnd = function (e) {
+  this.onSelect(e.changedTouches[0], "click");
+
+  e.event.preventDefault();
+};
+
 UranusHelperEntityPicker.prototype.onResize = function (width, height) {
   this.picker.resize(width, height);
 };
 
-UranusHelperEntityPicker.prototype.onSelect = function (event) {
+UranusHelperEntityPicker.prototype.onSelect = function (event, clickType) {
   var camera = this.camera.camera;
   var scene = this.app.scene;
   var picker = this.picker;
 
   picker.prepare(camera, scene);
 
-  var selected = picker.getSelection(event.x, event.y);
+  var selected = picker.getSelection(
+    event.x * this.app.graphicsDevice.maxPixelRatio,
+    event.y * this.app.graphicsDevice.maxPixelRatio
+  );
 
   if (selected[0]) {
     // Get the graph node used by the selected mesh instance
@@ -59,7 +88,8 @@ UranusHelperEntityPicker.prototype.onSelect = function (event) {
       entity = entity.getParent();
     }
     if (entity && (!this.pickTag || entity.tags.has(this.pickTag) === true)) {
-      this.app.fire(this.pickEvent, entity);
+      console.log(this.pickEvent, entity.name, clickType);
+      this.app.fire(this.pickEvent, entity, clickType);
     }
   }
 };
